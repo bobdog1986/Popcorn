b0b3b91a21d30abc958d0dca74ff9f8f57db0c42
Benjamin Bougot <bbougot@gmail.com>
2017-05-23 14:05:41 +0200

    Improve performances

--- Popcorn/UserControls/Home/Movie/Tabs/MovieTab.xaml.cs
+++ Popcorn/UserControls/Home/Movie/Tabs/MovieTab.xaml.cs
@@ -54,7 +54,7 @@ namespace Popcorn.UserControls.Home.Movie.Tabs
         private async void ScrollViewerScrollChanged(object sender, ScrollChangedEventArgs e)
         {
             var totalHeight = e.VerticalOffset + e.ViewportHeight;
-            if (totalHeight < 2d / 3d * e.ExtentHeight)
+            if (e.VerticalChange < 0 || totalHeight < 2d / 3d * e.ExtentHeight)
             {
                 return;
             }
